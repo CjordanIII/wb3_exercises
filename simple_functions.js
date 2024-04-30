aea4474a72e83635d0b447053c8f5a4dfe3ea33e
Clarence Jordan <clarencejordan087@gmail.com>
2024-04-30 11:59:45 -0500

    simple funcions

---
+++ simple_functions.js
@@ -0,0 +1,13 @@
+const favoritethings = () => {
+  console.log("clarence");
+  console.log("iron man");
+  console.log("rod wave");
+};
+
+const whyImHere = () => {
+  console.log("I'm here to learn more and take advantage of the internship");
+};
+
+const favoritePlace = () => {
+  console.log("disny world because it is amazing");
+};
