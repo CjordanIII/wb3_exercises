6c6879aa14585435c87523593a52709215494f1b
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-02 10:10:26 -0500

    done

--- advanced_parsing.js
+++ advanced_parsing.js
@@ -2,23 +2,28 @@
 
 function parseAndDisplayName(name) {
   const newName = name.trim().split(" ");
+  //   declaration
   let firstname;
   let middlename;
   let lastname;
   let sentence;
+  //   fisrt and last name
   if (newName.length == 2) {
     firstname = newName[0];
     lastname = newName[1];
     sentence = `First name :${firstname} \nLast name: ${lastname}`;
   } else if (newName.length === 3) {
+    // fisrt middle last
     firstname = newName[0];
     middlename = newName[1];
     lastname = newName[2];
     sentence = `First name :${firstname} \nMiddle name: ${middlename} \nLast name: ${lastname}`;
   } else {
+    // first name
     firstname = newName[0];
-    sentence = `First name: ${firstname}`;
+    sentence = `Only name: ${firstname}`;
   }
+  //   results
   console.log(sentence);
 }
 
