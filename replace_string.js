b922af434bba8098569a0bde5955826e7de4848e
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-02 14:48:52 -0500

    done

--- replace_string.js
+++ replace_string.js
@@ -1,7 +1,7 @@
 let message = "out corporate office are located in dallas";
 console.log(message.replace("dallas", "Austen"));
 
-// or
+console.log("================================ or =========================");
 
 message1 = "out corporate office are located in dallas";
 console.log(message1.replace(/dalLas/i, "Austen"));
